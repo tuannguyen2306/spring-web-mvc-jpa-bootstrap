9f5dca94142d3785311de6e472001c5641394c48
E6430 <trantuannguen2306@gmail.com>
2017-06-22 18:44:28 -0700

    code finish of java final

--- src/main/java/com/spring/bookstore/controller/BookController.java
+++ src/main/java/com/spring/bookstore/controller/BookController.java
@@ -25,7 +25,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class BookController {
     @Autowired
     BookRepository bookRepository;
-
+//
     @Autowired
     CategoryRepository categoryRepository;
 
